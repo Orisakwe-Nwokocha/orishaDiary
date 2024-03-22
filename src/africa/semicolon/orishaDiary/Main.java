c56da326965abd5ebcbbc8d64649bc94a042f150
Orisakwe Nwokocha <orisakwenwokocha1@gmail.com>
2024-03-22 11:09:31 +0100

    refactored

--- src/africa/semicolon/orishaDiary/Main.java
+++ src/africa/semicolon/orishaDiary/Main.java
@@ -1,7 +1,6 @@
 package africa.semicolon.orishaDiary;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.SpringApplicationHook;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
